63ab433791faca1dd2ad661ad7289f25e9c4d189
MikelArinaMarcos <mikel.arina@estudiantat.upc.edu>
2021-01-08 09:31:55 +0100

    Al pulsar boton pasa a segunda actividad

--- app/src/main/java/edu/upc/dsa/ejemplobasico2/MainActivity.java
+++ app/src/main/java/edu/upc/dsa/ejemplobasico2/MainActivity.java
@@ -2,6 +2,7 @@ package edu.upc.dsa.ejemplobasico2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void followerClick(android.view.View v) {
+        Intent intent = new Intent(MainActivity.this, infoFollowers.class);
+        startActivity(intent);;
     }
 }
